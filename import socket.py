fb8ea384384fc78d3146cdc4efd37a174d88302a
AhmedAmirMansy <116837949+AhmedAmirMansy@users.noreply.github.com>
2023-10-15 03:02:41 +0300

    import socket.py

--- import socket.py
+++ import socket.py
@@ -6,10 +6,11 @@ port=4007
 client_socket.connect(('127.0.0.1',port)) 
 while True:
  message=input("enter your message: ")
- client_socket.send(message.encode('utf-8'))
- if message=='CLOSE SOCKET':
+ client_socket.send(message.encode())
+ if message=="CLOSE SOCKET":
   client_socket.close()
   break
  else:
-   data =client_socket.recv(4007).decode('utf-8')
- 
+     client_socket.send(bytes(message.encode()))
+     data =client_socket.recv(1024).decode()
+     print(data)
